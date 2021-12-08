7b351b1077fe27efef881fe42981f2a420e56fa2
gabrielligoski <gabriel.ligoski@gmail.com>
2021-12-08 18:23:49 -0300

    CRUD & security!!! :sparkles:

--- src/main/java/com/surittec/surittec_challenge_backend/security/SecurityConfig.java
+++ src/main/java/com/surittec/surittec_challenge_backend/security/SecurityConfig.java
@@ -4,10 +4,15 @@ import com.surittec.surittec_challenge_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -18,6 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+        http
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .addLogoutHandler(new SecurityContextLogoutHandler())
+                );
     }
 
     @Autowired
--- src/main/java/com/surittec/surittec_challenge_backend/web/rest/PersonResource.java
+++ src/main/java/com/surittec/surittec_challenge_backend/web/rest/PersonResource.java
@@ -4,26 +4,21 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
-
 import javax.validation.Valid;
 
 import com.surittec.surittec_challenge_backend.model.Person;
-import com.surittec.surittec_challenge_backend.model.dto.PersonDTO;
-import com.surittec.surittec_challenge_backend.model.mapper.PersonMapper;
 import com.surittec.surittec_challenge_backend.repository.PersonRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api")
@@ -35,32 +30,59 @@ public class PersonResource {
         this.personRepository = personRepository;
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("/person/{id}")
     public ResponseEntity<Object> getPerson(@PathVariable Long id) {
-        System.out.printf("REST request to get Person : {%s}%n", id);
+        System.out.printf("REST request to get a Person : {%s}%n", id);
         Optional<Person> person = personRepository.findById(id);
-        if(person.isEmpty())
+        if (person.isEmpty())
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         return ResponseEntity.status(HttpStatus.OK).body(person);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("/person")
-    public ResponseEntity<List<Person>> getAllPersons(){
+    public ResponseEntity<List<Person>> getAllPersons() {
         System.out.print("REST request to get all Persons");
 
         return ResponseEntity.ok().body(personRepository.findAll());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/person")
-    public ResponseEntity<Person> createPerson(@Valid @RequestBody PersonDTO person) throws URISyntaxException {
-        System.out.print("REST request to post Person");
-
-        System.out.print(person.getName());
-        var result = personRepository.save(PersonMapper.INSTANCE.personDTOToPerson(person));
-        System.out.print(result.getName());
+    public ResponseEntity<Person> createPerson(@Valid @RequestBody Person person) throws URISyntaxException {
+        System.out.print("REST request to create Person");
+        var result = personRepository.save(person);
 
         return ResponseEntity.created(new URI("/api/person/" + result.getId()))
                 .body(result);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/person/{id}")
+    public ResponseEntity<Person> updatePerson(@Valid @RequestBody Person person) throws URISyntaxException {
+        System.out.print("REST request to update a Person");
+        var result = personRepository.save(person);
+        if (result.getId() == null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        if (personRepository.findById(person.getId()).isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        personRepository.findById(person.getId()).map(p -> {
+            p.setAddress(person.getAddress());
+            p.setCpf(person.getCpf());
+            p.setEmail(person.getEmail());
+            p.setName(person.getName());
+            p.setPhone(person.getPhone());
+            return null;
+        });
+        return ResponseEntity.created(new URI("/api/person/" + result.getId()))
+                .body(result);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/person/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        System.out.print("REST request to delete a Person");
+        personRepository.deleteById(id);
+    }
 }
